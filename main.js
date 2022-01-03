0b34cd33355b34eea559b7d9efae01e37d24c75a
FalakTheCoder <92030342+FalakTheCoder@users.noreply.github.com>
2022-01-03 14:50:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ function startClassification(){
 function modelReady(){
     classifier.classify(gotResults);
 }
+function gotResults(error, results){
+    if(error){
+        console.error(error);
+    } else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_accuracy").innerHTML = 'Accuracy - ' + (results[0].confidence*100).toFixed(2)+ '%';
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        document.getElementById("result_accuracy").style.color = "rgb("+random_number_r+", "+random_number_g+", "+random_number_b+")";
+        img = document.getElementById("animal");
+        
+
+        if(results[0].label == "Barking"){
+            img.src = "Dog.jpg";
+        } else if(results[0].label == "Meowing"){
+            img.src = "Cat.jpg";
+        }else if(results[0].label == "Mooing"){
+            img.src = "Cow.jpg";
+        } else if(results[0].label == "Roar"){
+            img.src = "Lion.jpg";
+        }
+    }
+}
